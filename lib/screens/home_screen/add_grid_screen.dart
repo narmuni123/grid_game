f8125960a3eeaa1e838cb238e2ec3bf4963c4945
narmuni123 <narmuni123@gmail.com>
2023-02-11 11:08:18 +0530

    Added text formfields in the add grid screen

--- lib/screens/home_screen/add_grid_screen.dart
+++ lib/screens/home_screen/add_grid_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:grid_game/constant/methods.dart';
 import 'package:grid_game/constant/size_constant.dart';
 
 class AddGridScreen extends StatefulWidget {
@@ -9,6 +11,18 @@ class AddGridScreen extends StatefulWidget {
 }
 
 class _AddGridScreenState extends State<AddGridScreen> {
+  TextEditingController rowController = TextEditingController();
+  TextEditingController columnController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    rowController.dispose();
+    columnController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +36,52 @@ class _AddGridScreenState extends State<AddGridScreen> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: width(context) * 0.1),
-          child: Column(
-            children: [
-              TextFormField(),
-              heightBox(height(context) * 0.02),
-              TextFormField(),
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                heightBox(height(context) * 0.02),
+                const Text(
+                  "Rows",
+                ),
+                TextFormField(
+                  controller: rowController,
+                  validator: rowValidator(),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+                heightBox(height(context) * 0.02),
+                const Text(
+                  "Columns",
+                ),
+                TextFormField(
+                  controller: columnController,
+                  validator: columnValidator(),
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  keyboardType: TextInputType.number,
+                  onFieldSubmitted: (_) => FocusScope.of(context).unfocus(),
+                ),
+                heightBox(height(context) * 0.02),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                    }
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Add Grid",
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),

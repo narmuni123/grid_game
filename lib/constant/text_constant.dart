8a1b9a968f2e19004c517f7a365e921da5cf28c0
narmuni123 <narmuni123@gmail.com>
2023-02-11 10:42:17 +0530

    Added splash screen

---
+++ lib/constant/text_constant.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+class TextConstants {
+// title name used in main file in material app widget title
+  static const String APP_NAME = "Griddy";
+
+// Navigation routes //
+  static const String SPLASH_ROUTE = "/splashScreen";
+  static const String ADD_GRID_ROUTE = "/addGrid";
+
+}
